62447a4e11c6842ad691c2d4351c50e1865d4c03
Jack <jackplummer78@gmail.com>
2023-01-03 17:56:00 +0000

    add meals section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Header from "./components/Layout/Header/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
-  return <Header />;
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
 }
 
 export default App;
